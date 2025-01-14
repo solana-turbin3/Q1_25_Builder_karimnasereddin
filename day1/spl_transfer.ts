688b5f5809a93b3b20d94a1f311112ac103dd725
karimnasereddin <twochopstick@PC>
2025-01-14 19:00:14 +0300

    added spl_transfer functions

---
+++ day1/spl_transfer.ts
@@ -0,0 +1,30 @@
+import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
+import wallet from "../wba-wallet.json"
+import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+
+// We're going to import our keypair from the wallet file
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+//Create a Solana devnet connection
+const commitment: Commitment = "confirmed";
+const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+// Mint address
+const mint = new PublicKey("8hS7Hv2kA3igy9RFXXeyNrK9uup58oVmrNjUKcuvw7z6");
+
+// Recipient address
+const to = new PublicKey("CpEBx9kZRYyicMXqGJRc5pNdkDGa7mgGFKXgyUGJsocr");
+
+(async () => {
+    try {
+        const fromTA = await getOrCreateAssociatedTokenAccount(connection,keypair,mint,keypair.publicKey);
+
+        const toTA = await getOrCreateAssociatedTokenAccount(connection,keypair,mint,to);
+
+        // Transfer the new token to the "toTokenAccount" we just created
+        const signature= await transfer(connection,keypair,fromTA.address,toTA.address,keypair.publicKey,1998);
+        console.log(`This is the transfer transaction: https://solscan.io/tx/${signature}?cluster=devnet`);
+    } catch(e) {
+        console.error(`Oops, something went wrong: ${e}`)
+    }
+})();
